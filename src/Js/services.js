27390a3c9d494613607870554e223c6464c8c0ea
georgee <jurajbosy1@gmail.com>
2022-11-13 00:51:24 +0100

    bug fixed with Service Section... -_- after 2 days...

--- src/Js/services.js
+++ src/Js/services.js
@@ -4,15 +4,35 @@ let TopSlider = document.querySelector('#top_slider');
 let TopSlider2 = document.querySelector('#top_slider2');
 let BottomSlider = document.querySelector('#bottom_slider');
 
-let h2_first_text  = document.querySelector('.text_h2');
-let h2_first_text_animation = document.querySelector('.animation_h2');
-let h1_text = document.querySelector('.h1_text_big');
+let service_Section = document.querySelector('.servicesSection');
+let h4 = document.querySelector('h4');
+let h4_first_text = document.querySelector('#first_text_container');
+
+// let h2_text = document.querySelector('.text_h2');
+const h2_text = document.querySelector('.text_h2'), ease = 0.05 , start_position = h2_text.offsetTop;
 
 window.addEventListener('scroll', () => {
     const scrollValue = window.scrollY;
 
+    if(scrollValue > 600){
+        service_Section.style.backgroundColor = 'white';
+        h2_text.classList.remove('text_h2')
+        h2_text.classList.add('fixed');
+
+        if(scrollValue < 850){
+            service_Section.style.backgroundColor = '#0d1b36';
+            h2_text.classList.remove('fixed');
+            h2_text.classList.add('text_h2');
+        }
+    }
+
+    if(scrollValue > 650){
+        h4_first_text.style.color = 'green';
+        h4_first_text.style.display = 'block';
+        h4_first_text.style.position = 'fixed';
+    }
+
     console.log(scrollValue);
 
-    
-
 });
+
